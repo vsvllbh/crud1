3cf0814ab34a1d810914a5998babb5516b71a247
Vasu Vallabh <vsvllbh143@gmail.com>
2019-10-09 21:10:55 +0530

    Create operations with toggle view of status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
-import { Component } from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
+import {Component} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
-import { map } from 'rxjs/operators';
-import {stringify} from 'querystring';
+import {map} from 'rxjs/operators';
 
 export interface Todo {
-  id?: string;
   description: string;
   completed: boolean;
 }
@@ -18,21 +16,39 @@ export interface Todo {
 export class AppComponent {
   todoCollectionRef: AngularFirestoreCollection<Todo>;
   todo$: Observable<Todo[]>;
-
-  constructor(private db: AngularFirestore) {
+  description = '';
+  isChecked = false;
+  constructor(public db: AngularFirestore) {
     this.todoCollectionRef = this.db.collection<Todo>('todos');
-    this.todo$ = this.todoCollectionRef.snapshotChanges().pipe(map(actions => {
+    this.todo$ = this.todo$ = this.todoCollectionRef.snapshotChanges().pipe(map(actions => {
       return actions.map(action => {
         const data = action.payload.doc.data() as Todo;
         const id = action.payload.doc.id;
-        console.log( 'Todo data: ' + stringify(data));
-        return { id, ...data };
+       // console.log('Todo data: ' + stringify(data));
+        return {id, ...data};
       });
     }));
+
   }
-  addTodo(todoDesc: string) {
-    if (todoDesc && todoDesc.trim().length) {
-      this.todoCollectionRef.add({ description: todoDesc, completed: false });
-    }
+    onSubmit() {
+   // console.log('description:' + this.description + this.isChecked);
+
+      // tslint:disable-next-line:only-arrow-functions
+    this.db.collection<Todo>('todos').add({description: this.description, completed: this.isChecked}).then(function(docRef) {
+     // console.log('Document written with ID: ', docRef.id);
+    })
+    // tslint:disable-next-line:only-arrow-functions
+      .catch(function(error) {
+       // console.error('Error adding document: ', error);
+      });
+    this.description = '';
+    this.isChecked = false;
+  }
+
+  onUpdate() {
+    /*const alovelaceDocumentRef = this.db.collection('todos').doc('7ucnl8Xnekr1zcVhsscB');
+    console.log(`Path of document is ${alovelaceDocumentRef}`);*/
+    console.log('On Update Invoked');
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,39 @@ import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import {AngularFireModule} from '@angular/fire';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatListModule} from '@angular/material/list';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatCardModule,
+    MatInputModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule.enablePersistence(),
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatListModule,
+    ScrollingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
